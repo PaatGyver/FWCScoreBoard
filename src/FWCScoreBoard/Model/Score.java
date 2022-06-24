428d6b983083768edd258b0b08b53b3b067f208d
DESKTOP-CFN6II8\acord <paatman@gmail.com>
2022-06-24 04:52:05 +0200

    Score class added

--- src/FWCScoreBoard/Model/Score.java
+++ src/FWCScoreBoard/Model/Score.java
@@ -4,9 +4,49 @@
 package FWCScoreBoard.Model;
 
 /**
- * @author acord
- *
+ * @author Patricio Moreno Losana
+ * Calss to represent score of a match
  */
 public class Score {
+	
+	private int homeGoals;
+	private int awayGoals;
+	
+	/**
+	 * Constructor of the class, score starts in 0 - 0 if no parameters added
+	 */
+	public Score() {
+		this.homeGoals = 0;
+		this.awayGoals = 0;
+	}
+	/**
+	 * Constructor of the class, score starts with goals added by parameters
+	 */
+	public Score(int homeGoals, int awayGoals) {
+		this.homeGoals = homeGoals;
+		this.awayGoals = awayGoals;
+	}
+	
+	public int getHomeGoals() {
+		return homeGoals;
+	}
+
+	public void setHomeGoals(int homeGoals) {
+		this.homeGoals = homeGoals;
+	}
+
+	public int getAwayGoals() {
+		return awayGoals;
+	}
+
+	public void setAwayGoals(int awayGoals) {
+		this.awayGoals = awayGoals;
+	}
+
+	@Override
+	public String toString() {
+		return homeGoals + " - " + awayGoals;
+	}
+
 
 }
